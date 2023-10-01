3dc08e1adca4b2c0c0847fca82a8b8fbd2fdfcee
m4x <test@local.com>
2023-10-01 20:02:20 +1000

    remove Base class to other module Some typing fixes

--- app/db/database.py
+++ app/db/database.py
@@ -1,39 +1,19 @@
-from typing import Any
-
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine, async_sessionmaker
-from sqlalchemy.orm import as_declarative, declared_attr
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 from app.core.security import settings
+from app.db.models.base import Base
 
 
-@as_declarative()
-class Base(object):
-    id: Any
-    __name__: str
-
-    @declared_attr
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
-
-
-def create_engine(testing_flag=settings.TESTING) -> AsyncEngine:
-    if testing_flag:
-        result_engine = create_async_engine(settings.DATABASE_URL_FOR_TEST, echo=True)
-    else:
-        result_engine = create_async_engine(settings.DATABASE_URL, echo=False)
-    return result_engine
-
-
-engine = create_engine()
+engine = create_async_engine(settings.DATABASE_URL, echo=False)
 async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def init_models():
+async def init_db():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
