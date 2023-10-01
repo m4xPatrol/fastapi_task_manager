4b6a90c435741b555eccb3ba8c34d14fd6eb91c7
m4x <test@local.com>
2023-10-01 20:08:05 +1000

    import fix CRUD usage for DB access

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -7,46 +7,38 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 
-from app.api.schemas.user import UserModel, UserInputModel
+from app.api.schemas.user import UserModel, UserCreate
 from app.core.security import get_password_hash, create_access_token, get_user_by_token, verify_password
 from app.db.database import get_session
-from app.api.models.user import User
+from app.db.models.user import User
 
+from app.db.crud.user import user_crud
 
 router = APIRouter()
 
 
 @router.post("/register", response_model=UserModel)
-async def create_user(user: UserInputModel, db: AsyncSession = Depends(get_session)):
-    db_user = User(username=user.username,
-                   full_name=user.full_name,
-                   email=user.email,
-                   age=user.age,
-                   hashed_password=get_password_hash(user.password))
-    db.add(db_user)
+async def create_user(user: UserCreate, db: AsyncSession = Depends(get_session)):
     try:
-        await db.commit()
-        return db_user
-    except IntegrityError as e:
-        await db.rollback()
+        user_db = await user_crud.add_user(db, user)
+        return user_db
+    except IntegrityError:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username or email was already registered.")
 
 
 @router.post("/login")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: AsyncSession = Depends(get_session)):
-    user = await db.execute(select(User).where(User.username == form_data.username))
-    user = user.scalars().first()
-    if not user or not verify_password(form_data.password, user.hashed_password):
+    user_db = await user_crud.get_user(db, form_data.username)
+    if not user_db or not verify_password(form_data.password, user_db.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password is incorrect.",
                             headers={"WWW-Authenticate": "Bearer"})
-    jwt_token = create_access_token({"sub": form_data.username})
+    jwt_token = create_access_token({"sub": user_db.username})
     return {"access_token": jwt_token, "token_type": "bearer"}
 
 
 @router.get("/about_me", response_model=UserModel)
 async def read_user(db: AsyncSession = Depends(get_session), username: str = Depends(get_user_by_token)):
-    user = await db.execute(select(User).where(User.username == username))
-    user = user.scalars().first()
-    if user is None:
+    user_db = await user_crud.get_user(db, username)
+    if user_db is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    return user
+    return user_db
