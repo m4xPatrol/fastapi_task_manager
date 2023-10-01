99ca4438e9f6d1fd2091e726af13b7199f01f972
m4x <test@local.com>
2023-10-01 20:06:15 +1000

    import fix

--- app/db/models/task.py
+++ app/db/models/task.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 
-from app.db.database import Base
+from app.db.models.base import Base
 
 
 class Task(Base):
